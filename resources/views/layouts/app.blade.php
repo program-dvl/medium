c01d5bff80ad2edc5f9aa6626cbffa0fac0dcf6c
Dhaval <dhaval.prajapati333@gmail.com>
2019-03-21 03:19:31 +0530

    admin url changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,8 +41,8 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @if(!Auth::check())
-                            <li><a class="nav-link" href="{{ url('/login') }}">Login</a></li>
-                            <li><a class="nav-link" href="{{ url('/register') }}">Register</a></li>
+                            <li><a class="nav-link" href="{{ url('/admin/login') }}">Login</a></li>
+                            <li><a class="nav-link" href="{{ url('/admin/register') }}">Register</a></li>
                         @else
 
                             <li class="nav-item dropdown">
